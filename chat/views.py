215bc8d988d362682bf7179c9c2ab4e10142c2f6
joseph reymond <josephreymond2011@gmail.com>
2020-05-18 11:50:20 +0530

    added consumers

--- chat/views.py
+++ chat/views.py
@@ -7,6 +7,9 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'chat/index.html')
 
+def newfox(request):
+    return render(request, 'chat/index.html')
+
 def room(request, room_name):
     return render(request, 'chat/room.html', {
         'room_name': room_name
