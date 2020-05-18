7d9e412f596dfef1ff95911dc45f1b294ce5fa52
joseph reymond <josephreymond2011@gmail.com>
2020-05-18 12:06:49 +0530

    added consumers

---
+++ chat/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class plan(models.Model):
+    plan_name= models.CharField(max_length=100,null=True)
+    plan_id = models.CharField(max_length=100,unique=True,null=True)
+    users = models.CharField(max_length=100,null=True)
+    devices = models.IntegerField(null=True)
+    expand_upto = models.IntegerField(null=True)
+    price = models.FloatField(default=0)
+    add = models.FloatField(default=0)
+    total_price = models.FloatField(null=True)
+    subscription_time = models.DurationField(null=True)
+    yearly = models.BooleanField(default=False)
